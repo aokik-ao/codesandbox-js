3f9ecdda69bf5cf56bd6a6680f9406f7d8cd2706
aokik-ao <61224695+aokik-ao@users.noreply.github.com>
2022-05-22 18:16:55 +0900

    一時コミット

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
-import "./styles.css";
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "aoki",
+//   age: 28
+// };
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = ["aoki", 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+// 複数変数を同時に宣言できるよ
+
+// mapはindexも保持出来る
+const nameArr = ["青木", "田中", "鈴木"];
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}`));
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "青木") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(newNameArr);
